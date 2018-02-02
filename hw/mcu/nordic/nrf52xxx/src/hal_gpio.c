057d18694d22af9a00c5b00cb2faa9f3f3fbc86e
Miguel Azevedo <miguellazev@gmail.com>
2018-02-02 12:09:04 -0200

    fixed hal_gpio_read function, spi tested on nrf52840

--- hw/mcu/nordic/nrf52xxx/src/hal_gpio.c
+++ hw/mcu/nordic/nrf52xxx/src/hal_gpio.c
@@ -47,6 +47,7 @@
  *  number is used unexpected and/or erroneous behavior will result.
  */
 #ifdef NRF52
+#define HAL_GPIO_INDEX(pin)
 #define HAL_GPIO_PORT(pin)      (NRF_P0)
 #define HAL_GPIO_MASK(pin)      (1 << pin)
 #define HAL_GPIOTE_PIN_MASK     GPIOTE_CONFIG_PSEL_Msk
@@ -170,9 +171,9 @@ hal_gpio_read(int pin)
     NRF_GPIO_Type *port;
     port = HAL_GPIO_PORT(pin);
 
-    return (port->PIN_CNF[pin] == GPIO_PIN_CNF_DIR_Output) ?
-         ((port->OUT & HAL_GPIO_MASK(pin))) :
-         ((port->IN & HAL_GPIO_MASK(pin)));
+    return (port->DIR & HAL_GPIO_MASK(pin)) ?
+        (port->OUT >> HAL_GPIO_INDEX(pin)) & 1UL :
+        (port->IN >> HAL_GPIO_INDEX(pin)) & 1UL;
 }
 
 /**
